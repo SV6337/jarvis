9a69a66fc0130339048a32312931183643b2f959
SV6337 <152479966+SV6337@users.noreply.github.com>
2024-09-21 15:36:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,15 +17,15 @@ function wishMe(){
     var hour = day.getHours();
 
     if(hour>=0 && hour<12){
-        speak("Good Morning Boss...")
+        speak("Good Morning Vasishta...")
     }
 
     else if(hour>12 && hour<17){
-        speak("Good Afternoon Master...")
+        speak("Good Afternoon sathvik Vasishta...")
     }
 
     else{
-        speak("Good Evenining Sir...")
+        speak("Good Evenining Sathvik Vasishta...")
     }
 
 }
